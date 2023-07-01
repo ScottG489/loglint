06f78c067353c0fbe91440a6e3b341d5da7e8972
Scott Giminiani <scottg489@gmail.com>
2023-07-01 13:34:39 -0400

    Add initial implementation
    Has a simple hard-coded rule that is printed out if matched

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+type rule struct {
+	name         string
+	shortReason  string
+	regexPattern string
+}
 
 func main() {
-    fmt.Println("hello world")
+	fileContents := getLogFileContents()
+
+	regexRules := getRules()
+
+	for _, rule := range regexRules {
+		pattern := regexp.MustCompilePOSIX(rule.regexPattern)
+		match := pattern.Match([]byte(fileContents))
+		if match {
+			fmt.Println(fmt.Sprintf("%s: %s", rule.name, string(pattern.Find([]byte(fileContents)))))
+		}
+	}
+}
+
+func getRules() []rule {
+	return []rule{
+		{"Generic warning", "Warnings are bad.", ".*warning:.*"},
+	}
+}
+
+func getLogFileContents() string {
+	arg := os.Args[1]
+	dat, err := os.ReadFile(arg)
+	check(err)
+	fileContents := string(dat)
+	return fileContents
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
